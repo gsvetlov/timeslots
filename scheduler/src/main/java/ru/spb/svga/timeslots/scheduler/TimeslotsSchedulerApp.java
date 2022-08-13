f15af79dcec953a57688e81c7f6eb5f034bcbdf1
gsvetlov <68912910+gsvetlov@users.noreply.github.com>
2022-08-13 13:40:38 +0300

    Merge pull request #2
    * checkstyle
    
    * add checkstyle support

--- scheduler/src/main/java/ru/spb/svga/timeslots/scheduler/TimeslotsSchedulerApp.java
+++ scheduler/src/main/java/ru/spb/svga/timeslots/scheduler/TimeslotsSchedulerApp.java
@@ -1,6 +1,5 @@
 package ru.spb.svga.timeslots.scheduler;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,11 +7,14 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @Slf4j
 @SpringBootApplication
-@RequiredArgsConstructor
 public class TimeslotsSchedulerApp {
 
     private static final String APP_START = "timeslot.scheduler.starting";
     private static final String APP_STOP = "timeslot.scheduler.stopping";
+
+    private TimeslotsSchedulerApp() {
+        throw new UnsupportedOperationException();
+    }
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(TimeslotsSchedulerApp.class);
